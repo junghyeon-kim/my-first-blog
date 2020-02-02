0361350be187931d73ad74a8e33deeda8c0f457a
Junghyun Kim <bluekim1993@gmail.com>
2020-02-02 14:17:41 +0900

    Added login/logout, comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,10 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('draft/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/puslish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('post/<int:pk>/commnet/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
